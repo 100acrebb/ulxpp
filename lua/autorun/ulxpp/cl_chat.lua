e3fe75206855134d7656197a07478a1eaa82c57e
RoboDerpy <roboderpy@gmail.com>
2016-05-22 16:11:00 +0700

    Updated commands auto complete

--- lua/autorun/ulxpp/cl_chat.lua
+++ lua/autorun/ulxpp/cl_chat.lua
@@ -21,6 +21,34 @@ limitations under the License.
 
 local ENABLE = CreateConVar('ulx_autocomplete', '1', FCVAR_ARCHIVE, 'Enable chat autocomplete')
 
+local function RealSplit(str, ignore)
+	local len = #str
+	local reply = {}
+	local TOKEN_LEVEL = 0
+	local currentstr = ''
+	
+	for i = 1, len do
+		local char = str[i]
+		if not ignore and char == '-' then break end
+		
+		if char == ' ' and TOKEN_LEVEL == 0 then
+			table.insert(reply, currentstr)
+			currentstr = ''
+			continue
+		elseif char == '<' then
+			TOKEN_LEVEL = TOKEN_LEVEL + 1
+		elseif char == '>' or char == ']' then --[<Some text: <Some Val>] --!!!, > last one is missing
+			TOKEN_LEVEL = math.max(TOKEN_LEVEL - 1, 0)
+		end
+		
+		currentstr = currentstr .. char
+	end
+	
+	table.insert(reply, currentstr)
+	
+	return reply
+end
+
 local ShouldDraw = false
 
 local function StartChat()
@@ -36,6 +64,7 @@ end
 local Drawn = {}
 local curcommand = ''
 local curtoken = ''
+local COMMAND_IS_VALID = false
 
 local function ChatTextChanged(str)
 	if not ENABLE:GetBool() then return end
@@ -62,16 +91,36 @@ local function ChatTextChanged(str)
 	
 	local ply = LocalPlayer()
 	local total = 0
+	local playerTarget = false
+	local targetPly
+	
 	for k, v in pairs(Found) do
 		local access = ULib.ucl.query(ply, v.cmd)
 		if not access then continue end
+		
+		playerTarget = false
 		
 		if total > 10 then
 			table.insert(Drawn, '<...>')
 			break
 		end
 		
-		local format = token .. ULXPP.UnpackCommand(v.cmd) .. ' ' .. v:getUsage(ply)
+		local usage = v:getUsage(ply)
+		local usplit = RealSplit(usage, true)
+		
+		for k, v in pairs(usplit) do
+			if v == '-' then break end
+			if v == '<player>' or v == '<players>' then 
+				playerTarget = true 
+				targetPly = split[k + 1]
+			end
+			
+			if split[k + 1] then
+				usplit[k] = split[k + 1]
+			end
+		end
+		
+		local format = token .. ULXPP.UnpackCommand(v.cmd) .. ' ' .. table.concat(usplit, ' ')
 		if table.HasValue(Drawn, format) then continue end --eh
 		total = total + 1
 		
@@ -82,9 +131,44 @@ local function ChatTextChanged(str)
 	
 	if total == 0 then
 		Drawn[1] = 'No ULX command match.'
+		COMMAND_IS_VALID = false
 	elseif total == 1 then
 		Drawn[1] = 'ULX Chat Command, to paste all missed arguments press TAB.'
 		curcommand = last
+		COMMAND_IS_VALID = true
+		
+		local hit = false
+		
+		if playerTarget then
+			local target = targetPly and targetPly ~= '' and targetPly ~= ' ' and string.lower(targetPly)
+			
+			for k, v in pairs(player.GetAll()) do
+				if target then 
+					if not string.find(string.lower(v:Nick()), target) then continue end
+				end
+				
+				local return_value, msg = hook.Run(ULib.HOOK_PLAYER_TARGET, LocalPlayer(), curcommand, v)
+				if return_value == false then continue end
+				
+				if not hit then
+					hit = true
+					
+					if not target then
+						table.insert(Drawn, 'Valid targets:')
+					else
+						table.insert(Drawn, 'Match targets:')
+					end
+				end
+				
+				table.insert(Drawn, v:Nick())
+			end
+			
+			if not hit then
+				table.insert(Drawn, 'No match')
+			end
+		end
+	else
+		COMMAND_IS_VALID = false
 	end
 end
 
@@ -121,13 +205,11 @@ end
 local function OnChatTab(str)
 	if not ENABLE:GetBool() then return end
 	if not ShouldDraw then return end
-	if #Drawn == 0 then return end
-	if #Drawn == 1 then return end
-	if #Drawn > 3 then return end
+	if not COMMAND_IS_VALID then return end
 	
 	local command = Drawn[2]
 	local split1 = string.Explode(' ', str)
-	local split2 = string.Explode(' ', command)
+	local split2 = RealSplit(command)
 	
 	split1[1] = curtoken .. curcommand
 	local output = table.concat(split1, ' ')
